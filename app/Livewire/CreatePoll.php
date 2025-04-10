dc4815822ff2823a04ad3e14fe654ae1682598d1
syazwan <28nursyazwan@gmail.com>
2025-04-10 18:03:58 +0800

    create title and option feature

--- app/Livewire/CreatePoll.php
+++ app/Livewire/CreatePoll.php
@@ -8,9 +8,22 @@ class CreatePoll extends Component
 {
 
     public $title;
+    public $options = ['first'];
 
     public function render()
     {
         return view('livewire.create-poll');
     }
+
+    public function addOption()
+    {
+        $this->options[] = '';  //add new value in options array
+    }
+
+    public function removeOption($index)
+    {
+        unset($this->options[$index]);  //remove value from options array
+        $this->options = array_values($this->options);  //re-index the array
+    }
+
 }
